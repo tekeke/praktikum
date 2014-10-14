32fc0cc91ef6d796a1b9ea4dc250a063b1147264
tekeke <tom.szy@yahoo.de>
2014-10-14 10:43:54 +0200

    git_intro - Hello World Nachricht

--- git_intro/src/de/uni_koeln/spinfo/git_intro/Intro.java
+++ git_intro/src/de/uni_koeln/spinfo/git_intro/Intro.java
@@ -3,7 +3,10 @@ package de.uni_koeln.spinfo.git_intro;
 public class Intro {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String hello = "Hello World!";
+		
+		System.out.println(hello);
 
 	}
 
